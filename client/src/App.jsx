14c42f3737ecff599c72d0a172311ede49849e4a
Alvaro Torres <atorr1020@gmail.com>
2024-09-29 13:43:49 -0500

    updated client side folder structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
 import './App.css'
-import NavBar from './NavBar'
+
+// Component Imports
+import NavBar from './assets/components/layout/NavBar'
 
 function App() {
 
