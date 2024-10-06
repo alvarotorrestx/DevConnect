8494d6003623ec4b02c1d393ea78498a3fde80c4
Alvaro Torres <alvaro@alvarotorres.dev>
2024-10-05 19:57:55 -0500

    Temporarily redefined theme config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -16,6 +16,27 @@ module.exports = {
   ],
   daisyui: {
     styled: true,
-    themes: ['nord', 'dim'],
+    themes: [
+      {
+        nord: {
+          ...require("daisyui/src/theming/themes")["nord"],
+          info: "#28ebff",
+          success: "#62efbd",
+          error: "#bf616a",
+        },
+      },
+      {
+        dim: {
+          ...require("daisyui/src/theming/themes")["dim"],
+          primary: "#5e81ac",
+          "primary-content": "#03060b",
+          secondary: "#81a1c1",
+          "secondary-content": "#060e10",
+          accent: "#88c0d0",
+          neutral: "#4c566a",
+          "--rounded-btn": "0.25rem"
+        },
+      },
+    ]
   }
 }
