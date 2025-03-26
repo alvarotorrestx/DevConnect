abfca6e588c84ef9396d685a4726e1e94f8bc28c
Alvaro Torres <alvaro@alvarotorres.dev>
2025-03-26 01:25:38 -0500

    updated user controller, added prevention error catches, saves to db now

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,14 +1,32 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 const registerUser = async (req, res) => {
     try {
-        res.json({
-            "email": req.body.email,
-            "username": req.body.username,
-            "firstName": req.body.firstName,
-            "lastName": req.body.lastName,
-            "password": req.body.password,
-        })
+        const { email, username, firstName, lastName, password } = req.body;
+
+        // Ensure all fields are entered to register
+        if (!email || !username || !firstName || !lastName || !password) return res.status(400).json({ "message": "All fields are required to register." });
+
+        // Check if user already exists
+        const duplicateUser = await User.findOne({ $or: [{ email }, { username }] });
+        if (duplicateUser) return res.status(409).json({ message: "Email or username already in use." });
+
+        // Hash password with bcrypt 10 salt rounds
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+        const newUser = await User.create({
+            email,
+            username,
+            firstName,
+            lastName,
+            password: hashedPassword,
+        });
+
+        // Remove password before sending back
+        const { password: _, ...userData } = newUser.toObject();
+
+        res.status(201).json(userData);
     }
     catch (err) {
         res.status(400).json({ error: err.message });
