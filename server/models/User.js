d1b01d9bbc137e91ebb3cdabc161b558c5207bd7
Alvaro Torres <alvaro@alvarotorres.dev>
2025-03-29 01:58:06 -0500

    Added Cors Options for backend access routes and allow frontend form to register user

--- server/models/User.js
+++ server/models/User.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     username: { type: String, required: true, unique: true },
-    firstName: { type: String, required: true, unique: true },
-    lastName: { type: String, required: true, unique: true },
-    password: { type: String, required: true, },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    password: { type: String, required: true },
     role: {
         type: String,
         enum: ['user', 'admin', 'superadmin', 'owner'],
