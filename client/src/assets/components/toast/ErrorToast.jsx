1e3e41f7262065b86783b462dc6d8d5870384839
Alvaro Torres <79110517+alvarotorrestx@users.noreply.github.com>
2025-04-09 09:23:51 -0500

    Merge pull request #77 from sudarshansangle56/toast-success-component
    Add reusable ErrorToast and SuccessToast components

---
+++ client/src/assets/components/toast/ErrorToast.jsx
@@ -0,0 +1,33 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { Alert } from "react-daisyui";
+
+const ErrorToast = ({ message, show, icon, iconBgColor, status, onClose }) => {
+  return (
+    <AnimatePresence>
+      {show && (
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -50 }}
+          transition={{ duration: 0.4 }}
+          className="fixed top-5  transform -translate-x-1/2 z-50 w-[55%] "
+        >
+          <Alert className="alert alert-error shadow-lg relative pr-2 rounded-[8px]">
+
+            {icon && (
+             <div className="rounded-full bg-white">
+                {icon}
+              </div>
+              
+            )}
+<span className="font-medium text-[19px]">
+              {message}
+            </span>
+          </Alert>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default ErrorToast;
