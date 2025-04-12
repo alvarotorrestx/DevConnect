ccba18fa36ddd3746c2dbcfeeaa4177f48a78ad7
Alvaro Torres <alvaro@alvarotorres.dev>
2025-04-12 16:43:34 -0500

    quick text color update on unauthorized

--- client/src/assets/components/subcomponents/Unauthorized.jsx
+++ client/src/assets/components/subcomponents/Unauthorized.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Unauthorized = () => {
     return (
         <div className="text-center mt-10">
-            <h1 className="text-3xl font-bold text-red-500">403 - Unauthorized</h1>
+            <h1 className="text-3xl font-bold text-error">403 - Unauthorized</h1>
             <p className="mt-4">You do not have permission to view this page.</p>
         </div>
     );
