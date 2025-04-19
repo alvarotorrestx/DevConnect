82d6c19be6607fe573ec1c1e88b93f3775f7a57c
sudarshan sangle <darshsangle9456@gmail.com>
2025-04-19 23:45:21 +0530

    changes are done

--- client/src/assets/components/profile/Blog/Blog.jsx
+++ client/src/assets/components/profile/Blog/Blog.jsx
@@ -6,8 +6,7 @@ function Blog() {
     <div className="max-w-[90%] lg:max-w-4xl mx-auto p-6 bg-base-100 rounded-lg shadow-md mt-10 relative">
       <h1 className='font-bold'>My Posts / Blog</h1>
       <br />
-      <div className='flex items-center justify-center gap-[40px]'>
-      <Card/>
+      <div className='flex items-center justify-center gap-[10px] md:gap-[40px]'>
       <Card/>
       <Card/>
       </div>
--- client/src/assets/components/profile/Blog/Card.jsx
+++ client/src/assets/components/profile/Blog/Card.jsx
@@ -1,10 +1,9 @@
 function Card() {
   return (
     <div className="min-h-[200px] p-3 flex flex-col justify-between rounded-sm shadow-md w-[250px] bg-base-300">
- <h3 className="text-lg text-center text-base-content opacity-70 font-medium">
-  IT Is Going To Boom 2026
-</h3>
-
+      <h3 className="text-lg text-center text-base-content opacity-70 font-medium">
+        IT Is Going To Boom 2026
+      </h3>
 
       <p className="text-sm text-center text-base-content/80">
         The IT sector is expected to witness exponential growth driven by AI,
