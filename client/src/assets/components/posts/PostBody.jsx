19c44f1eea5469722e33040bf1793a37a979b248
Priyadharshini0101 <priyadharshinis5102@gmail.com>
2025-05-06 10:31:09 +0530

    Fixed Posts body render their HTML name or codes instead of displaying symbols

--- client/src/assets/components/posts/PostBody.jsx
+++ client/src/assets/components/posts/PostBody.jsx
@@ -1,28 +1,28 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import he from 'he';
+import ReactMarkdown from 'react-markdown';
 
 const PostBody = ({ body }) => {
     const [expandedPost, setExpandedPost] = useState(false);
 
-    const renderBodyWithTags = (text) => {
-        return text.split(' ').map((word, i) => {
-            if (word.startsWith('#')) {
-                const cleanTag = word.replace(/[^\w#]/g, '').slice(1).toLowerCase();
-                return (
-                    <Link
-                        key={i}
-                        // to={`/tags/${cleanTag}`}
-                        to={`#`}
-                        className="text-primary hover:underline"
-                    >
-                        {word + ' '}
-                    </Link>
-                );
-            }
-            return <span key={i}>{word + ' '}</span>;
-        });
-    };
+    // const renderBodyWithTags = (text) => {
+    //     return text.split(' ').map((word, i) => {
+    //         if (word.startsWith('#')) {
+    //             const cleanTag = word.replace(/[^\w#]/g, '').slice(1).toLowerCase();
+    //             return (
+    //                 <Link
+    //                     key={i}
+    //                     // to={`/tags/${cleanTag}`}
+    //                     to={`#`}
+    //                     className="text-primary hover:underline"
+    //                 >
+    //                     {word + ' '}
+    //                 </Link>
+    //             );
+    //         }
+    //         return <span key={i}>{word + ' '}</span>;
+    //     });
+    // };
 
     const shouldTruncate = body.split('\n').length > 3 || body.length > 300;
 
@@ -30,7 +30,17 @@ const PostBody = ({ body }) => {
         <div>
 
             <p className={`${expandedPost ? '' : 'line-clamp-3'} whitespace-pre-line`}>
-                {renderBodyWithTags(he.decode(body))}
+              <ReactMarkdown
+                components={{
+                  a: ({ href, children }) => (
+                    <Link to={href} className='text-primary hover:underline'>
+                      {children}
+                    </Link>
+                  ),
+                }}
+              >
+                {renderBodyWithTags(body)}
+              </ReactMarkdown>
             </p>
 
             {shouldTruncate && (
