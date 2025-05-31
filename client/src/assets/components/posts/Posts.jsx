73284c5916facf73cd47ed88e96d2a0ed937404a
Alvaro Torres <atorr1020@gmail.com>
2025-05-30 23:52:47 -0500

    Improved area of loading spinner on pages and updated bg color of post feeds

--- client/src/assets/components/posts/Posts.jsx
+++ client/src/assets/components/posts/Posts.jsx
@@ -52,7 +52,7 @@ const Posts = () => {
                 });
 
                 setPosts(response.data.posts);
-                (posts.length <=0) && setCurrentPage(1);
+                (posts.length <= 0) && setCurrentPage(1);
                 setTotalPages(response.data.totalPages);
             } catch (err) {
                 console.log(err);
@@ -62,10 +62,10 @@ const Posts = () => {
         };
         // scrollY = window.scrollY;
         window.scrollTo({
-            top:0,
-            behavior:'smooth'
+            top: 0,
+            behavior: 'smooth'
         })
-        
+
         fetchPosts(currentPage);
     }, [auth?.accessToken, currentPage, posts.length]);
 
@@ -90,125 +90,129 @@ const Posts = () => {
 
     return (
         <div className="mx-auto rounded-lg">
-
-            <CreatePost POST_URL={POST_URL} auth={auth} setPosts={setPosts} />
-
             {
                 loading
                     ?
                     <Loading />
                     :
-                    posts.length > 0 ? (
-                        posts.map((post) => (
-                            <div key={post._id} className="relative [&:not(:last-child)]:mb-6 p-5 rounded-md shadow-md border border-base-300 bg-base-100">
+                    (
+                        <>
+                            <CreatePost POST_URL={POST_URL} auth={auth} setPosts={setPosts} />
 
-                                {canUserModifyPost(post, auth) &&
-                                    (
-                                        <>
-                                            <div className='absolute top-0 right-0 flex flex-row justify-center items-center gap-2 mt-3 mr-3'>
-                                                <EditPost
-                                                    auth={auth}
-                                                    post={post}
-                                                    POST_URL={POST_URL}
-                                                    showSuccess={showSuccess}
-                                                    showError={showError}
-                                                    setPosts={setPosts}
-                                                />
+                            {posts.length > 0 ? (
+                                posts.map((post) => (
+                                    <div key={post._id} className="relative [&:not(:last-child)]:mb-6 p-5 rounded-md shadow-md border border-base-300 bg-base-100">
 
-                                                <DeletePost
-                                                    auth={auth}
-                                                    post={post}
-                                                    POST_URL={POST_URL}
-                                                    showSuccess={showSuccess}
-                                                    showError={showError}
-                                                    setPosts={setPosts}
-                                                />
+                                        {canUserModifyPost(post, auth) &&
+                                            (
+                                                <>
+                                                    <div className='absolute top-0 right-0 flex flex-row justify-center items-center gap-2 mt-3 mr-3'>
+                                                        <EditPost
+                                                            auth={auth}
+                                                            post={post}
+                                                            POST_URL={POST_URL}
+                                                            showSuccess={showSuccess}
+                                                            showError={showError}
+                                                            setPosts={setPosts}
+                                                        />
+
+                                                        <DeletePost
+                                                            auth={auth}
+                                                            post={post}
+                                                            POST_URL={POST_URL}
+                                                            showSuccess={showSuccess}
+                                                            showError={showError}
+                                                            setPosts={setPosts}
+                                                        />
+                                                    </div>
+                                                </>
+                                            )
+                                        }
+
+                                        {/* Author Info */}
+                                        <div className="flex items-center gap-4 mb-2">
+                                            <img
+                                                src={post.author.avatar}
+                                                alt="avatar"
+                                                className="w-10 h-10 rounded-full"
+                                            />
+                                            <div>
+                                                <p className="font-semibold">{post.author.firstName} {post.author.lastName}</p>
+                                                <Link
+                                                    to={`/profile/${post.author.username}`}
+                                                    className="text-sm link link-primary link-hover"
+                                                >
+                                                    @{post.author.username}
+                                                </Link>
                                             </div>
-                                        </>
-                                    )
-                                }
+                                        </div>
 
-                                {/* Author Info */}
-                                <div className="flex items-center gap-4 mb-2">
-                                    <img
-                                        src={post.author.avatar}
-                                        alt="avatar"
-                                        className="w-10 h-10 rounded-full"
-                                    />
-                                    <div>
-                                        <p className="font-semibold">{post.author.firstName} {post.author.lastName}</p>
-                                        <Link
-                                            to={`/profile/${post.author.username}`}
-                                            className="text-sm link link-primary link-hover"
-                                        >
-                                            @{post.author.username}
-                                        </Link>
-                                    </div>
-                                </div>
-
-                                {/* Post Body */}
-                                <PostBody body={post.body} />
+                                        {/* Post Body */}
+                                        <PostBody body={post.body} />
 
 
-                                {/* Media */}
-                                {post.media && (
-                                    <div className="mt-4 space-y-4">
+                                        {/* Media */}
+                                        {post.media && (
+                                            <div className="mt-4 space-y-4">
 
-                                        {/* Render Images */}
-                                        {Array.isArray(post.media.images) && post.media.images.length > 0 && (
-                                            <div className="space-y-2">
-                                                {post.media.images.map((url, index) => (
-                                                    <img
-                                                        key={`image-${index}`}
-                                                        src={url}
-                                                        alt={`Post image ${index + 1}`}
-                                                        className="rounded-md max-w-full"
-                                                    />
-                                                ))}
+                                                {/* Render Images */}
+                                                {Array.isArray(post.media.images) && post.media.images.length > 0 && (
+                                                    <div className="space-y-2">
+                                                        {post.media.images.map((url, index) => (
+                                                            <img
+                                                                key={`image-${index}`}
+                                                                src={url}
+                                                                alt={`Post image ${index + 1}`}
+                                                                className="rounded-md max-w-full"
+                                                            />
+                                                        ))}
+                                                    </div>
+                                                )}
+
+                                                {/* Render Videos */}
+                                                {Array.isArray(post.media.videos) && post.media.videos.length > 0 && (
+                                                    <div className="space-y-2">
+                                                        {post.media.videos.map((url, index) => (
+                                                            <video
+                                                                key={`video-${index}`}
+                                                                controls
+                                                                className="rounded-md max-w-full"
+                                                            >
+                                                                <source src={url} />
+                                                                Your browser does not support the video tag.
+                                                            </video>
+                                                        ))}
+                                                    </div>
+                                                )}
+
                                             </div>
                                         )}
 
-                                        {/* Render Videos */}
-                                        {Array.isArray(post.media.videos) && post.media.videos.length > 0 && (
-                                            <div className="space-y-2">
-                                                {post.media.videos.map((url, index) => (
-                                                    <video
-                                                        key={`video-${index}`}
-                                                        controls
-                                                        className="rounded-md max-w-full"
-                                                    >
-                                                        <source src={url} />
-                                                        Your browser does not support the video tag.
-                                                    </video>
-                                                ))}
-                                            </div>
+
+                                        {/* Tags */}
+                                        {post.tags.length > 0 && (
+                                            <p className="text-sm text-base-content/70 my-2">
+                                                <strong>Tags:</strong> {post.tags.map(tag => `#${tag}`).join(' ')}
+                                            </p>
                                         )}
 
+                                        {/* Post Meta Info */}
+                                        <div className="text-xs text-base-content/60 flex flex-wrap gap-4 mt-2">
+                                            <p>📅 {new Date(post.createdAt).toLocaleString()}</p>
+                                            <p>🔥 Featured: {post.featured ? 'Yes' : 'No'}</p>
+                                            <p>❤️ Likes: {post.likes.length}</p>
+                                            <p>💬 Comments: {post.comments.length}</p>
+                                            {post.repostedBy.length > 0 && <p>🔁 Reposts: {post.repostedBy.length}</p>}
+                                        </div>
+
                                     </div>
-                                )}
-
-
-                                {/* Tags */}
-                                {post.tags.length > 0 && (
-                                    <p className="text-sm text-base-content/70 my-2">
-                                        <strong>Tags:</strong> {post.tags.map(tag => `#${tag}`).join(' ')}
-                                    </p>
-                                )}
-
-                                {/* Post Meta Info */}
-                                <div className="text-xs text-base-content/60 flex flex-wrap gap-4 mt-2">
-                                    <p>📅 {new Date(post.createdAt).toLocaleString()}</p>
-                                    <p>🔥 Featured: {post.featured ? 'Yes' : 'No'}</p>
-                                    <p>❤️ Likes: {post.likes.length}</p>
-                                    <p>💬 Comments: {post.comments.length}</p>
-                                    {post.repostedBy.length > 0 && <p>🔁 Reposts: {post.repostedBy.length}</p>}
-                                </div>
-
-                            </div>
-                        ))
+                                ))
+                            )
+                                :
+                                <p>No posts found.</p>
+                            }
+                        </>
                     )
-                        :
-                        <p>No posts found.</p>
             }
 
 
@@ -229,7 +233,7 @@ const Posts = () => {
             ) : (totalPages > 4 && (
                 <div className="flex justify-center mt-6">
                     <div className="join">
-                        <button className={`join-item btn `} onClick={() => setCurrentPage(currentPage- 2)}>
+                        <button className={`join-item btn `} onClick={() => setCurrentPage(currentPage - 2)}>
                             {currentPage - 2}
                         </button>
                         <button className={`join-item btn`} onClick={() => setCurrentPage(currentPage - 1)}>
@@ -244,7 +248,7 @@ const Posts = () => {
                         </button>
                     </div>
                 </div>
-                )
+            )
             )}
 
             {/* Add Toast Components */}
