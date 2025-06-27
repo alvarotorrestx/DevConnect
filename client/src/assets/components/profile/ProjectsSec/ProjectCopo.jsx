6da68b4d1e283d19d3768068c1b6efe357fee3c8
Kotla.Lokeshwari <kotlalokeshwari1871@gmail.com>
2025-06-27 19:01:27 +0530

    Made changes in ProjectCopo.jsx and ProjectModal.jsx

--- client/src/assets/components/profile/ProjectsSec/ProjectCopo.jsx
+++ client/src/assets/components/profile/ProjectsSec/ProjectCopo.jsx
@@ -42,36 +42,37 @@ function ProjectCopo({ project, onEdit, canEdit ,onDelete }) {
             <h1 className="font-bold text-xl mt-2">{project.title}</h1>
 
             <h3 className="font-semibold">
-              Tech Stack:
+              Tech Stack:{" "}
               <span className="font-normal">{project.techStack}</span>
             </h3>
             <h3 className="font-semibold">
-              Description:
+              Description:{" "}
               <span className="font-normal">{project.description}</span>
             </h3>
           </div>
 
           <div className="flex gap-5">
+            {project.sourceCodeLink && 
             <a
               href={project.sourceCodeLink}
               target="_blank"
               rel="noopener noreferrer"
             >
               <FaGithub size={30} color="black" />
-            </a>
-            <a
+            </a>}
+            {project.liveLink && <a
               href={project.liveLink}
               target="_blank"
               rel="noopener noreferrer"
               className="flex items-center gap-2 text-black hover:underline text-2xl"
             >
               <FaGlobe />
-            </a>
+            </a>}
           </div>
         </div>
         <div className="right flex mt-2 flex-col items-end mr-[18%]">
           <h3 className="sm:mr-6">{project.duration}</h3>
-          {project?.media?.images && (
+          {project?.media?.images[0] && (
             <img
               className="h-[150px] mt-2 rounded sm:mr-5"
               src={project.media.images[0]}
--- client/src/assets/components/profile/ProjectsSec/ProjectModal.jsx
+++ client/src/assets/components/profile/ProjectsSec/ProjectModal.jsx
@@ -1,10 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { axiosPrivate } from "../../../../api/axios";
 import useAuth from "../../../../auth/useAuth";
+import { FaTimesCircle,FaCheckCircle } from "react-icons/fa";
+
+// Toast imports
+import ErrorToast from "../../toast/ErrorToast";
+import { useErrorToast } from "../../toast/useErrorToast";
+import SuccessToast from "../../toast/SuccessToast";
+import { useSuccessToast } from "../../toast/useSuccessToast";
 
 function ProjectModal({ onClose, onSave, initialData }) {
   const { auth, setAuth } = useAuth();
   const [type, setType] = useState(initialData ? "update" : "Add");
+  const projectNameRef = useRef(null);
+  const errRef = useRef();
+
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
   const [form, setForm] = useState({
     id: initialData?._id || "",
     title: initialData?.title || "",
@@ -16,28 +29,104 @@ function ProjectModal({ onClose, onSave, initialData }) {
     techStack: initialData?.techStack || [],
   });
 
-  console.log("initial data", initialData);
+  const {
+    message: errorMessage,
+    show: showErrorToast,
+    showError,
+  } = useErrorToast();
+
+  const {
+    message: successMessage,
+    show: showSuccessToast,
+    showSuccess,
+  } = useSuccessToast();
+
+  const [validTitle, setValidTitle] = useState(false);
+  const [validDuration, setValidDuration] = useState(false);
+  const [validDescription, setValidDescription] = useState(false);
+  const [validSource, setValidSource] = useState(false);
+  const [validLive, setValidLive] = useState(false);
+  const [validTechStack, setValidTechStack] = useState(false);
+
+  const TITLE_REGEX = /^[\w\s\-]{3,100}$/;
+  const DURATION_REGEX = /^[a-zA-Z]+-\d{4}-[a-zA-Z]+-\d{4}$/;
+  const DESCRIPTION_REGEX = /^.{10,}$/;
+  const URL_REGEX =
+    /^(https?:\/\/)?([\w-]+\.)+[\w-]+(\/[\w\-._~:/?#[\]@!$&'()*+,;=]*)?$/;
+  const TECH_STACK_ITEM_REGEX = /^[a-zA-Z0-9.+#-]{2,20}$/;
+
+  useEffect(() => {
+    projectNameRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setValidTitle(TITLE_REGEX.test(form.title));
+    setValidDuration(DURATION_REGEX.test(form.duration));
+    setValidDescription(DESCRIPTION_REGEX.test(form.description));
+    setValidSource(URL_REGEX.test(form.sourceCodeLink));
+    setValidLive(URL_REGEX.test(form.liveLink));
+    setValidTechStack(
+      form.techStack.every((t) => TECH_STACK_ITEM_REGEX.test(t))
+    );
+  }, [form]);
+
   const handleChange = (e) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
+    const { name, value } = e.target;
+    if (name === "techStack") {
+      setForm({
+        ...form,
+        techStack: value
+          .split(",")
+          
+      });
+    } else {
+      setForm({ ...form, [name]: value });
+    }
   };
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [
+    form.id,
+    form.title,
+    form.duration,
+    form.description,
+    form.media,
+    form.sourceCodeLink,
+    form.liveLink,
+    form.techStack,
+  ]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (type === "Add") {
-      console.log(form);
+      const v1 = TITLE_REGEX.test(form.title);
+      const v2 = DURATION_REGEX.test(form.duration);
+      const v3 = DESCRIPTION_REGEX.test(form.description);
+      const v4 = URL_REGEX.test(form.sourceCodeLink);
+      const v5 = URL_REGEX.test(form.liveLink);
+      const v6 = form.techStack.every((t) => TECH_STACK_ITEM_REGEX.test(t));
+
+      if (!v1 || !v2 || !v3 || !v4 || !v5 || !v6) {
+        showError("Invalid Entry");
+        return;
+      }
+
       try {
         const response = await axiosPrivate.post("/projects/create", form, {
           headers: {
             Authorization: `Bearer ${auth?.accessToken}`,
           },
         });
+        showSuccess("Project added Successfully!");
+        setSuccess(true);
       } catch (err) {
-        console.log(err);
+        showError(`${JSON.stringify(err.response.data.message).slice(1, -1)}`);
+
+        errRef.current.focus();
       }
       onSave(form);
-   
     } else if (type === "update") {
-   
       const updatedForm = { _id: form.id };
 
       for (const key in form) {
@@ -60,10 +149,12 @@ function ProjectModal({ onClose, onSave, initialData }) {
             },
           }
         );
-        onClose(false)
-        onSave(response.data)
+        onClose(false);
+        onSave(response.data);
+        showSuccess("Project updated Successfully!");
+        setSuccess(true);
       } catch (err) {
-        console.log(err);
+        showError(`${JSON.stringify(err.response.data.message).slice(1, -1)}`);
       }
     }
   };
@@ -93,6 +184,7 @@ function ProjectModal({ onClose, onSave, initialData }) {
             placeholder="Project Title"
             value={form.title}
             onChange={handleChange}
+            ref={projectNameRef}
             className="input input-bordered w-full bg-base-100 text-base-content"
           />
 
@@ -118,7 +210,7 @@ function ProjectModal({ onClose, onSave, initialData }) {
             type="text"
             name="techStack"
             placeholder="Skills (e.g., React, Node)"
-            value={form.techStack}
+            value={form.techStack.join(",")}
             onChange={handleChange}
             className="input input-bordered w-full bg-base-100 text-base-content"
           />
@@ -164,6 +256,21 @@ function ProjectModal({ onClose, onSave, initialData }) {
           </div>
         </form>
       </div>
+
+      <SuccessToast
+        message={successMessage}
+        show={showSuccessToast}
+        status="success"
+        icon={<FaCheckCircle className="text-green-600 text-4xl" />}
+        iconBgColor="bg-green-700"
+      />
+      <ErrorToast
+        message={errorMessage}
+        show={showErrorToast}
+        status="error"
+        icon={<FaTimesCircle className="text-red-600 text-4xl" />}
+        iconBgColor="bg-red-700"
+      />
     </div>
   );
 }
