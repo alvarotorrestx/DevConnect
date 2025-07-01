5fe55bed9aac3a99760bade6613beba6bc6f1d51
Alvaro Torres <alvaro@alvarotorres.dev>
2025-07-01 02:18:20 -0500

    Validation additions to createJob

--- server/controllers/jobsController.js
+++ server/controllers/jobsController.js
@@ -12,15 +12,31 @@ const createJob = async (req, res) => {
         const { title, description, company, location, remote, isSalary, salaryRange, hourlyRate, employmentType, requiredSkills, appLink } = req.body;
 
         // Ensure all fields are entered to create user
-        if (!title || !description || !company || !location || remote === undefined || isSalary === undefined || !(salaryRange || hourlyRate) || !employmentType || !requiredSkills) {
+        if (!title || !description || !company || !location || remote === undefined || isSalary === undefined || !(salaryRange || hourlyRate) || !employmentType) {
             return res.status(400).json({ message: "All fields are required to create job listing." });
         }
 
-        // TODO: Ensure salaryRange and hourlyRate not empty
-        // TODO: Add enhanced security measures
-        // Ensure req.body is of correct types
+        if (isSalary) {
+            if (!Array.isArray(salaryRange) || salaryRange.length !== 2 || !salaryRange.every(num => typeof num === 'number')) {
+                return res.status(422).json({ message: "Salary range must be an array of 2 numbers." });
+            }
+        } else {
+            if (!Array.isArray(hourlyRate) || hourlyRate.length !== 2 || !hourlyRate.every(num => typeof num === 'number')) {
+                return res.status(422).json({ message: "Hourly rate must be an array of 2 numbers." });
+            }
+        }
 
-        // TODO: Find satisfying slug layout
+        const allowedTypes = ['Full-time', 'Part-time', 'Contract', 'Internship', 'Temporary', 'Freelance'];
+
+        if (!Array.isArray(employmentType) || employmentType.length === 0 || !employmentType.every(type => allowedTypes.includes(type))) {
+            return res.status(422).json({ message: "Employment type must be an array of valid types." });
+        }
+
+        if (requiredSkills && !Array.isArray(requiredSkills)) {
+            return res.status(422).json({ message: "Skills must be an array." });
+        }
+
+        // Generate slug with layout of company/title_of_job/uuid
         const generateSlug = (title, company) => {
             const clean = (str) => str
                 .toLowerCase()
@@ -28,10 +44,10 @@ const createJob = async (req, res) => {
                 .replace(/[^a-z0-9]+/g, '-')
                 .replace(/^-+|-+$/g, '');
 
-            // Shortens the uuid
-            const uniqueId = uuidv4();
+            // Generate UUID and remove dashes
+            const uniqueId = uuidv4().replace(/-/g, '');
 
-            return `${clean(company)}-${clean(title)}-${uniqueId}`;
+            return `${clean(company)}/${clean(title)}/${uniqueId}`;
         };
 
         const slug = generateSlug(title, company);
